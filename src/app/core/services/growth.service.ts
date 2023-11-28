27b8da21553b416de7711f6798feac45c9ca7eef
Chanaka Lakshan Balasuriya <47913002+galactusclb@users.noreply.github.com>
2023-11-29 01:15:56 +0530

    implement functionalities in growth component

---
+++ src/app/core/services/growth.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { environment } from 'src/environments/environment';
+
+const BACKEND_URL = environment.backendUrl
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GrowthService {
+
+  constructor(private http: HttpClient) { }
+
+  connectToDevice(): Observable<boolean> {
+    return this.http.get<boolean>(BACKEND_URL + '/connect')
+  }
+
+  capture(): Observable<boolean> {
+    return this.http.get<boolean>(BACKEND_URL + '/capture')
+  }
+
+}
