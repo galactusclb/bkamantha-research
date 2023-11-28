4d80a685931dc52ee14f79c1a1d4945860ff47a6
bhanuka kamantha <50150022+bkamantha@users.noreply.github.com>
2023-11-29 00:46:06 +0530

    fix

--- src/app/modules/seed/create/create.component.ts
+++ src/app/modules/seed/create/create.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.scss']
+  styleUrls: ['./create.component.scss'],
 })
 export class CreateComponent {
+  constructor(private router: Router) {}
 
   submit() {
-    console.log("gg");
-
+    console.log('gg');
+    this.router.navigate(['../seeds']);
   }
 }
--- src/app/modules/seed/list/list.component.ts
+++ src/app/modules/seed/list/list.component.ts
@@ -1,25 +1,40 @@
-import { Component } from '@angular/core';
-import { map, tap } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { ISeed } from '@core/models/seed';
+import { BehaviorSubject, map, tap } from 'rxjs';
 import { SeedService } from 'src/app/services/seed.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent {
+  dates: number[] | undefined = Array.from(Array(12).keys());
 
-  dates: number[] | undefined = Array.from(Array(12).keys())
+  selectedDate?: number = 12;
 
-  selectedDate?: number = 12
+  rows: ISeed[] | undefined = [];
 
-  rows$ = this.seedsService.getSeeds().pipe(
-    map((items) => items?.results),
-  )
+  isLoading: boolean = false;
 
-  constructor(private seedsService: SeedService) { }
+  constructor(private seedsService: SeedService) {}
+
+  fetchData() {
+    this.isLoading = true;
+
+    this.seedsService.getSeeds().subscribe(
+      (res) => {
+        this.rows = res?.result;
+        this.isLoading = false;
+      },
+      (err) => {
+        console.error(err);
+        this.isLoading = false;
+      }
+    );
+  }
 
   onSelectDate(date: number) {
-    this.selectedDate = date
+    this.selectedDate = date;
   }
 }
